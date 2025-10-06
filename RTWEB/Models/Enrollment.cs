785bb57218feac85cb7499094fbcd6436d2fa952
MdLotif <lotif.oct@gmail.com>
2025-10-06 17:30:56 +0600

    Progress

--- RTWEB/Models/Enrollment.cs
+++ RTWEB/Models/Enrollment.cs
@@ -8,5 +8,9 @@
         public int? StudentId { get; set; }
         public int? CourseId { get; set; }
         public int? ScheduleId {  get; set; }
+        public decimal? TotalFee { get; set; }
+        public decimal? PaidAmount { get; set; }
+        public decimal? DueAmount { get; set; }
+        public int? Status { get; set; }
     }
 }
